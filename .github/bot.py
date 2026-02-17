915383e3d6a591bb2ebc5a639cfd005affa51452
Tools-app <localhost.hutao@gmail.com>
2026-02-17 20:18:56 +0800

    Revert "bot: only upload files"
    This reverts commit ec4bf09f8458993adf89613af582078d6d87dd51.

--- .github/bot.py
+++ .github/bot.py
@@ -9,7 +9,28 @@ API_HASH = "d524b414d21f4d37f08684c1df41ac9c"
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 CHAT_ID = int(os.environ.get("CHAT_ID"))
+COMMIT_URL = os.environ.get("COMMIT_URL")
+RUN_ID = int(os.environ.get("RUN_ID"))
+COMMIT_MESSAGE = os.environ.get("COMMIT_MESSAGE")
 BOT_CI_SESSION = os.environ.get("BOT_CI_SESSION")
+MSG_TEMPLATE = """
+New push to Github
+```
+{commit_message}
+```
+See commit detail [here]({commit_url})
+#ci_{run_id}
+""".strip()
+
+def get_caption():
+    msg = MSG_TEMPLATE.format(
+        commit_message=COMMIT_MESSAGE,
+        commit_url=COMMIT_URL,
+        run_id=RUN_ID,
+    )
+    if len(msg) > 1024:
+        return COMMIT_URL
+    return msg
 
 def get_zip_files():
     return glob.glob('./output/*.zip')
@@ -23,9 +44,13 @@ async def send_telegram_message():
         
     async with TelegramClient(StringSession(BOT_CI_SESSION), api_id=API_ID, api_hash=API_HASH) as client:
         await client.start(bot_token=BOT_TOKEN)
+        print("[+] Caption: ")
+        print(get_caption())
+        print("---")
         print("[+] Sending")
         await client.send_file(
             entity=CHAT_ID,
+            caption=get_caption(),
             file=zip_files,
             parse_mode="markdown",
         )
