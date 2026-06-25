465bbd6f8c48ae34f553ccfb9c8b106e38b7f154
生于生时 亡于亡刻 <127004703+Tools-cx-app@users.noreply.github.com>
2026-06-25 09:44:37 +0800

    refactor: refactor constant declarations (#120)
    Instead of hardcoding, it uses `const_format` to generate dynamically.

--- src/defs.rs
+++ src/defs.rs
@@ -1,17 +1,21 @@
 // Copyright (C) 2026 meta-magic_mount-rs developers
 // SPDX-License-Identifier: Apache-2.0
 
-pub const MODULE_PATH: &str = "/data/adb/modules/";
-pub const CUSTOM_LIST_PATH: &str = "/data/adb/magic_mount/custom";
+use const_format::concatcp;
+
+const ADB: &str = "/data/adb";
+pub const MODULE_PATH: &str = concatcp!(ADB, "/modules");
+const MAGIC_MOUNT: &str = concatcp!(ADB, "/magic_mount");
+pub const CUSTOM_LIST_PATH: &str = concatcp!(MAGIC_MOUNT, "/custom");
 pub const SELINUX_XATTR: &str = "security.selinux";
 pub const DISABLE_FILE_NAME: &str = "disable";
 pub const REMOVE_FILE_NAME: &str = "remove";
 pub const SKIP_MOUNT_FILE_NAME: &str = "skip_mount";
 pub const REPLACE_DIR_XATTR: &str = "trusted.overlay.opaque";
 pub const REPLACE_DIR_FILE_NAME: &str = ".replace";
-pub const CONFIG_FILE: &str = "/data/adb/magic_mount/config.toml";
+pub const CONFIG_FILE: &str = concatcp!(MAGIC_MOUNT, "/config.toml");
 pub const SELF_MODULE_PATH: &str = "/data/adb/modules/magic_mount_rs";
 pub const MACHIKADO_FILE: &str = "/data/adb/modules/magic_mount_rs/machikado";
 pub const MAZOKU_FILE: &str = "/data/adb/modules/magic_mount_rs/mazoku";
-pub const MODULE_PROP: &str = "/data/adb/modules/magic_mount_rs/module.prop";
-pub const MODULE_PROP_ORIG: &str = "/data/adb/modules/magic_mount_rs/module.prop.orig";
+pub const MODULE_PROP: &str = concatcp!(MODULE_PATH, "/", env!("MODULE_ID"), "/module.prop");
+pub const MODULE_PROP_ORIG: &str = concatcp!(MODULE_PATH, "/", env!("MODULE_ID"), "/module.prop.orig");
