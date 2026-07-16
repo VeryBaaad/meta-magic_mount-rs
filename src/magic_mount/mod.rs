ca37e942fabe25e6032a1e64234d7c495af195ec
Tools-cx-app <localhost.hutao@gmail.com>
2026-07-17 07:43:10 +0800

    mount: fix an issue where a regular file overwrite was incorrectly promoted to a tmpfs mount for the entire directory for magic_mount.

--- src/magic_mount/mod.rs
+++ src/magic_mount/mod.rs
@@ -146,7 +146,7 @@ impl MagicMount {
                     _ => {
                         if let Ok(metadata) = real_path.symlink_metadata() {
                             let file_type = NodeFileType::from(metadata.file_type());
-                            file_type != self.node.file_type || file_type == NodeFileType::Symlink
+                            file_type != node.file_type || file_type == NodeFileType::Symlink
                         } else {
                             // real path not exists
                             true
