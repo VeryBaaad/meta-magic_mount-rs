197176e08b31ba97d721d9a3331cb56bdf6d2ee3
Tools-cx-app <localhost.hutao@gmail.com>
2026-08-09 14:09:23 +0800

    license: fix header

--- webui/src/ui/miuix/App.vue
+++ webui/src/ui/miuix/App.vue
@@ -1,3 +1,9 @@
+<!--
+
+    Copyright (C) 2026 meta-magic_mount-rs developers
+    SPDX-License-Identifier: GPL-v3
+
+-->
 <script setup lang="ts">
 import { computed, onBeforeUnmount, ref } from "vue";
 import { useI18n } from "vue-i18n";
