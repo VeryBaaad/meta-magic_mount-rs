25a8532a747f6af6db7d5d00c3cbfad15ff1a010
VeryBaaad <verybaaad@outlook.com>
2026-08-10 14:36:24 +0800

    fix: remove git confidents

--- module/customize.sh
+++ module/customize.sh
@@ -49,11 +49,8 @@ extract 'config_apatch.toml'
 extract 'metainstall.sh'
 extract 'metamount.sh'
 extract 'metauninstall.sh'
-<<<<<<< HEAD
 extract "mazoku"
-=======
 extract 'emulated-soft-reboot.sh'
->>>>>>> 00f525c (mount: support late-load mode)
 extract 'uninstall.sh'
 extract 'launcher.png'
 do_extract true "$ZIPFILE" "bin/$ARCH_BINARY/magic_mount_rs" "$MODPATH" "mmrs"
